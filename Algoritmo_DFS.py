4035cb823128aad970ca372fcd91988a04997b7a
sfbarragan <sfbarragan@espe.edu.ec>
2022-05-29 15:37:49 -0500

    Se agrego el método agregar_borde

--- Algoritmo_DFS.py
+++ Algoritmo_DFS.py
@@ -31,7 +31,56 @@ class Grafo:
     Imprimir_lista_adyacencia(self)
         Imprime el grafo generado en base a la lista de adyacencia.
 
-    bfs_transversal(self, nodo_de_inicio)
+    dfs(self, inicio, objetivo, ruta = [], visitado = set())
         Método que imprime el recorrido BFS de un vértice fuente dado.
     """
 
+    def __init__(self, numero_nodos, dirigido=True):
+        """
+        Este metodo funcionara como el constuctor de la clase Grafo(), recibe el Numero de nodos (m_num_nodos),
+        crea el rango de nodos (numero_nodos), determina el tipo de grafo si es dirigido o no dirigido (m_dirigido) y
+        creara el diccionario de la lista de adyacencia.
+
+
+        Parametros
+        ----------
+        m_numero_nodos : int
+            Cantidad de nodos que tendra el grafo.
+        m_nodos : int
+            Rango de nodos sobre los que trabajara el grafo.
+        m_dirigido : boolean
+            Tipo de nodo dirigido o no dirigido.
+        m_lista_adyacencia : diccionario
+            Diccionario que almacena el valor de los nodos
+        """
+        # Se asigna el valor del numero de nodos a través del parametro recibido
+        self.m_numero_nodos = numero_nodos
+        # Se genera el rango de nodos en base a m_numero_nodos
+        self.m_nodos = range(self.m_numero_nodos)
+        # se define el tipo de grafo
+        self.m_dirigido = dirigido
+        # Se crea el diccionario de la lista de adyacencia
+        self.m_lista_adyacencia = {nodo: set() for nodo in self.m_nodos}
+
+   
+    def agregar_borde(self, nodo1, nodo2, peso=1):
+        """
+        Este método define el borde de la lista de adyacencia.
+        Recibe como parametros el nodo1, el nodo2 y el peso cuyo valor por defecto es de 1.
+        Posteriormente se agregan a la lista de adyacencia del nodo al que corresponde.
+
+        Parametros
+        ----------
+        nodo1 : int
+        nodo2 : int
+        peso: int
+
+        Retorno
+        -------
+        Nada 
+        """
+        # Agrega el nodo 2 a la lista de adyacencia del nodo 1.
+        self.m_lista_adyacencia[nodo1].add((nodo2, peso))
+        if not self.m_dirigido:
+            # Agrega el nodo 1 a la lista de adyacencia del nodo 2.
+            self.m_lista_adyacencia[nodo2].add((nodo1, peso))
